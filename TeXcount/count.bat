4f5d46e4a2eb4b40715aa12322ed617c86913262
H Semenenko <henry@semenenko.co.uk>
2019-02-17 18:21:28 +0000

    Remove the ignored directories

--- TeXcount/count.bat
+++
@@ -1,5 +0,0 @@
-set datetime=%date:~-4%_%date:~3,2%_%date:~0,2%__%time:~0,2%_%time:~3,2%_%time:~6,2%
-
-echo %datetime%
-
-perl texcount.pl -nosub -inc -stat -dir=../ -out=./History/%datetime%.txt ../henry_semenenko_thesis.tex
