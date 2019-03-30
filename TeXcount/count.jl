5efce5846b9cdbaa0a897eeec0215876de312d9d
H Semenenko <henry@semenenko.co.uk>
2019-03-30 23:41:21 +0000

    Updated the count.jl script to plot word, caption, figure and equation counts. Added some text and references to a few chapters

--- TeXcount/count.jl
+++ TeXcount/count.jl
@@ -1,17 +1,34 @@
 using Dates
+using Plots
+gr(size=(1920,1080))
 
-dir = "C:\\Users\\Henry\\Dropbox\\Bristol University Work\\Thesis\\"
+date = []
+text = []
+caption = []
+figure = []
+maths = []
 
-cd(dir)
+t_reg = r"Words in text: (.*\d)"
+c_reg = r"Words outside text \(captions, etc\.\): (.*\d)"
+f_reg = r"Number of floats/tables/figures: (.*\d)"
+i_reg = r"Number of math inlines: (.*\d)"
+d_reg = r"Number of math displayed: (.*\d)"
 
-cmd = `perl ./TeXcount/texcount.pl  -nosub -inc ./henry_semenenko_thesis.tex`
+for file in filter(x -> endswith(x, "txt"), readdir("./History"))
+    push!(date, Date(file[1:10],"y_m_d")) 
+    data = open("./History/" * file) do f
+        read(f, String)
+    end
+    t = parse(Int, collect(eachmatch(t_reg, data))[end][1])
+    push!(text, t)
+    c = parse(Int, collect(eachmatch(c_reg, data))[end][1])
+    push!(caption, c)
+    f = parse(Int, collect(eachmatch(f_reg, data))[end][1])
+    push!(figure, f)
+    m = parse(Int, collect(eachmatch(i_reg, data))[end][1]) + parse(Int, collect(eachmatch(d_reg, data))[end][1])
+    push!(maths, m)
+end
 
-stats = read(cmd)
-
-dir_history = dir * "\\TeXcount\\History\\" * Dates.format(now(), "yyyy_mm_dd_HHMM") * ".txt"
-
-file = open(dir_history, "w")
-
-write(file, stats)
-
-close(file)
+plot(date,[text,caption], lab = ["Text" "Captions"], w=3, legend = :topleft)
+plot!(twinx(), date, [figure, maths], lab = ["Figures" "Equations"], line=(:dot), w=3, legend = :topright)
+savefig("words.pdf")
