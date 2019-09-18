8ec7fd8af072c13b68f38c1ed00c401514a64e08
Henry Semenenko <hs15098@bristol.ac.uk>
2019-09-18 16:48:02 +0100

    Added frequency of works option

--- TeXcount/count.bat
+++ TeXcount/count.bat
@@ -2,4 +2,4 @@ set datetime=%date:~-4%_%date:~3,2%_%date:~0,2%__%time:~0,2%_%time:~3,2%_%time:~
 
 echo %datetime%
 
-perl texcount.pl -nosub -inc -stat -dir=../ -out=./History/%datetime%.txt ../henry_semenenko_thesis.tex
+perl texcount.pl -nosub -inc -freq=10 -stat -dir=../ -out=./History/%datetime%.txt ../henry_semenenko_thesis.tex
