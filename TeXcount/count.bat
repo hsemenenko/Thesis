5e9f79396fbd4994bdc5779ca3693b6c9ca0671e
Henry Semenenko <hs15098@bristol.ac.uk>
2019-10-16 14:13:44 +0100

    Changed Plots
    Added julia line to the bat file to plot in one click.
    
    Added milestones to the plotting.

--- TeXcount/count.bat
+++ TeXcount/count.bat
@@ -3,3 +3,5 @@ set datetime=%date:~-4%_%date:~3,2%_%date:~0,2%__%time:~0,2%_%time:~3,2%_%time:~
 echo %datetime%
 
 perl texcount.pl -nosub -inc -freq=10 -stat -dir=../ -out=./History/%datetime%.txt ../henry_semenenko_thesis.tex
+
+julia count.jl
--- TeXcount/count.jl
+++ TeXcount/count.jl
@@ -8,12 +8,16 @@ text = Int64[]
 caption = Int64[]
 figure = Int64[]
 maths = Int64[]
+words = Int64[]
 
 t_reg = r"Words in text: (.*\d)"
 c_reg = r"Words outside text \(captions, etc\.\): (.*\d)"
 f_reg = r"Number of floats/tables/figures: (.*\d)"
 i_reg = r"Number of math inlines: (.*\d)"
 d_reg = r"Number of math displayed: (.*\d)"
+w_reg = r"All words: (.*\d)"
+
+milestones = [Date("2019-10-11")]
 
 for file in filter(x -> endswith(x, "txt"), readdir("./History"))
     push!(date, Date(file[1:10],"yyyy_mm_dd"))
@@ -28,20 +32,30 @@ for file in filter(x -> endswith(x, "txt"), readdir("./History"))
     push!(figure, f)
     m = parse(Int, collect(eachmatch(i_reg, data))[end][1]) + parse(Int, collect(eachmatch(d_reg, data))[end][1])
     push!(maths, m)
+    w = parse(Int, collect(eachmatch(w_reg, data))[end][1])
+    push!(words, w)
 end
 
 plot(date,
-   [text,caption],
+   [text,caption,words],
    xrotation = 45,
-   lab = ["Text" "Captions"],
+   lab = ["Text" "Captions" "Total"],
    linetype=:steppost,
    w=3,
-   yaxis = ("Text and captions", (0,20000)),
+   yaxis = ("Text and captions", (0,25000)),
    yformatter = :plain,
    legend = :topleft,
    left_margin = 10mm,
    right_margin = 10mm
    )
+
+plot!([date[66],date[66]],
+        [0, words[66]],
+        st = :path,
+        label = ["Chapter 3 Draft"],
+        yformatter = :plain,
+        w=2,
+        color = :purple)
 
 plot!(twinx(),
    date,
