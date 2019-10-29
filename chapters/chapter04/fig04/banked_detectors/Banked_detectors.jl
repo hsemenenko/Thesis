e3fceed19998a9b2548cd5956c2fd7a3223f36e1
Henry Semenenko <hs15098@bristol.ac.uk>
2019-10-29 16:10:13 +0000

    Adding detail to chapter 4

--- chapters/chapter04/fig04/banked_detectors/Banked_detectors.jl
+++ chapters/chapter04/fig04/banked_detectors/Banked_detectors.jl
@@ -21,28 +21,38 @@ dt150 = [find_zero(x -> f(l, 150, 0.2, x), (0,1)) for l in 0:1:350]
 dt250 = [find_zero(x -> f(l, 250, 0.2, x), (0,1)) for l in 0:1:350]
 dt350 = [find_zero(x -> f(l, 350, 0.2, x), (0,1)) for l in 0:1:350]
 
-plot([dt0, dt25, dt100, dt250],
-    yaxis = ("Detection Probability", :log10, (1.5e-3,2e-1)),
+p1 = plot(dt0,
+    yaxis = ("Probability of Detection", :log10, (1.5e-3,2e-1)),
     xaxis = ("Distance (km)", (0,200)),
-    label = [0, 25, 100, 250],
-    color = RdYlPu4,
-    w = [2 1.5 1.5 1.5]
+    label = "",
+    color = RGB(0.2,0.2,0.2),
+    ls =:dash,
+    w = 3
+    )
+
+plot!([dt25, dt100, dt250],
+    yaxis = ("Probability of Detection", :log10, (1.5e-3,2e-1)),
+    xaxis = ("Distance (km)", (0,200)),
+    label = [25, 100, 250],
+    color = RdYlPu3,
+    ls = :solid,
+    w = 2
     )
 
 #Assumming a state rate of 250MHz (4ns spacing) and a dead time of 100 ns,
 #we can model that the detectors are dead for k=25 cycles
 init =
 
-coin2 = [k^2 for k in [find_zero(x -> f(l, 25, 0.2, x), (0,1)) for l in 0:1:350]]
-coin4 = [4*k^2*(1-k)^2 for k in [find_zero(x -> f(l, 25, 10^(-0.3) * 0.2, x), (0,1)) for l in 0:1:350]]
-coin8 = [16*k^2*(1-k)^6 for k in [find_zero(x -> f(l, 25, 10^(-0.6) * 0.2, x), (0,1)) for l in 0:1:350]]
-coin16 = [64*k^2*(1-k)^14 for k in [find_zero(x -> f(l, 25, 10^(-0.9) * 0.2, x), (0,1)) for l in 0:1:350]]
-coin32 = [256*k^2*(1-k)^30 for k in [find_zero(x -> f(l, 25, 10^(-1.2) * 0.2, x), (0,1)) for l in 0:1:350]]
-coin64 = [1024*k^2*(1-k)^30 for k in [find_zero(x -> f(l, 25, 10^(-1.5) * 0.2, x), (0,1)) for l in 0:1:350]]
+coin2 = [k^2 for k in [find_zero(x -> f(l, 200, 0.2, x), (0,1)) for l in 0:1:350]]
+coin4 = [4*k^2*(1-k)^2 for k in [find_zero(x -> f(l, 200, 10^(-0.3) * 0.2, x), (0,1)) for l in 0:1:350]]
+coin8 = [16*k^2*(1-k)^6 for k in [find_zero(x -> f(l, 200, 10^(-0.6) * 0.2, x), (0,1)) for l in 0:1:350]]
+coin16 = [64*k^2*(1-k)^14 for k in [find_zero(x -> f(l, 200, 10^(-0.9) * 0.2, x), (0,1)) for l in 0:1:350]]
+coin32 = [256*k^2*(1-k)^30 for k in [find_zero(x -> f(l, 200, 10^(-1.2) * 0.2, x), (0,1)) for l in 0:1:350]]
+coin64 = [1024*k^2*(1-k)^62 for k in [find_zero(x -> f(l, 200, 10^(-1.5) * 0.2, x), (0,1)) for l in 0:1:350]]
 
-plot(dt0.^2,
-    yaxis = ("Probability of Coincidence", :log10, (2e-5, 5e-2), [1e-2, 1e-3, 1e-4]),
-    xaxis = ("Distance", (0,150)),
+p2 = plot(dt0.^2,
+    yaxis = ("Probability of Coincidence", :log10, (2e-9, 5e-2)),
+    xaxis = ("Distance (km)", (0,350)),
     label = "",
     ls = :dash,
     color = RGB(0.2,0.2,0.2),
